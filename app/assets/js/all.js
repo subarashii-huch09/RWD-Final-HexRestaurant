8c8af77a8cca7d89ac11749c9a28e2deff07ec32
Tansy <cwhu823@icloud.com>
2021-10-31 01:09:56 +1300

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".navBtn").click(function (e) {
+    e.preventDefault();
+    $(".nav__list").toggleClass("active");
+  });
+});
+//# sourceMappingURL=all.js.map
