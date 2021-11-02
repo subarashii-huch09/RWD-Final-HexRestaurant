ff8fca81b9c3448e73963565db95c8a4b572dcd3
Tansy <cwhu823@icloud.com>
2021-11-02 22:09:42 +1300

    polish up effects

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,9 +1,31 @@
 "use strict";
 
 $(document).ready(function () {
+  // hamburger menu button
   $(".navBtn").click(function (e) {
     e.preventDefault();
     $(".nav__list").toggleClass("active");
   });
+
+
+  // go to top 
+  $(".go-top").click(function (event) {
+    $("html , body").animate({ scrollTop: 0 }, 700);
+  });
+
+
+  // parallax scrolling
+  $(".scrollTop").click(function (event) {
+    event.preventDefault();
+    let target = $(this).attr("href");
+    let targetPos = $(target).offset().top;
+    $("html , body").animate({ scrollTop: targetPos }, 1000);
+  });
+
+  
 });
-//# sourceMappingURL=all.js.map
+
+
+// aos
+
+AOS.init();
