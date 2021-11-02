fc42dfbfda0adc922e0e419bbac6fe2c5995375c
Tansy <cwhu823@icloud.com>
2021-11-02 22:10:34 +1300

    Update 2021-11-02T09:10:32.539Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,9 +1,27 @@
 "use strict";
 
 $(document).ready(function () {
+  // hamburger menu button
   $(".navBtn").click(function (e) {
     e.preventDefault();
     $(".nav__list").toggleClass("active");
+  }); // go to top 
+
+  $(".go-top").click(function (event) {
+    $("html , body").animate({
+      scrollTop: 0
+    }, 700);
+  }); // parallax scrolling
+
+  $(".scrollTop").click(function (event) {
+    event.preventDefault();
+    var target = $(this).attr("href");
+    var targetPos = $(target).offset().top;
+    $("html , body").animate({
+      scrollTop: targetPos
+    }, 1000);
   });
-});
+}); // aos
+
+AOS.init();
 //# sourceMappingURL=all.js.map
